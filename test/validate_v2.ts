a636708ec555fcdf522b3a49697b91c25c54a38d
Miguel de los Reyes <mdlreyes@indeed.com>
2026-04-17 14:30:25 -0400

    Add startup and standard ban lists 26.05

--- test/validate_v2.ts
+++ test/validate_v2.ts
@@ -523,7 +523,8 @@ describe('Restrictions', () => {
         'startup_ban_list_24_09_for_classic_only',
         'startup_balance_update_25_04_for_classic_only',
         'startup_balance_update_25_11_for_classic_only',
-        'startup_balance_update_26_03_for_classic_only'
+        'startup_balance_update_26_03_for_classic_only',
+        'startup_balance_update_26_05_for_classic_only'
       ]);
 
     restrictionIds.forEach((restrictionId) => {
